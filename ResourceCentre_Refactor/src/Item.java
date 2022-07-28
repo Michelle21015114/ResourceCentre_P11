a88bd4616ff24d5c48d7ddddf3b58623271ef8df
21015114 <21015114@DESKTOP-TO2PC7H>
2022-07-28 10:09:22 +0800

    Added comment for description

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -7,7 +7,7 @@ public class Item {
 
 	public Item(String assetTag, String description) {
 		this.assetTag = assetTag;
-		//mICHELLE - for CI exercise
+		//MICHELLE - for CI exercise
 		this.description = description;
 		this.dueDate = "";
 		this.isAvailable = true;
